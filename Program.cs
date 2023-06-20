33cfd81bc86c6f55928cf0069dbc0f2f10604f83
Malik Murray <130183731+murraymalik2@users.noreply.github.com>
2023-06-20 19:07:12 -0400

    "updated sugar"

--- Program.cs
+++ Program.cs
@@ -1,66 +1,24 @@
-﻿using System;
-using System.Linq.Expressions;
-
-namespace SelectionStatements
+﻿namespace SyntaxAndSyntaxSugarExercise
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //Number Guessing Game
-            //var r = new Random();
-            // favNumber = r.Next(1, 1000);
-            //var favNumber = 5;
+            // int answer = 4;
+            //explcit typing 
+            //string response; //explicit typing
+            //if (answer < 9)
+            // {
+            //     response = answer + " is less than nine";
+            // }
+            // else
+            // {
+            //    response = answer + " is greater than or equal to nine";
+            //}
 
-           // Console.WriteLine("Pick a number from 1-1000");
-            //var userInput = int.Parse(Console.ReadLine());
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+        }
 
-            //if (userInput < favNumber)
-            {
-                //Console.WriteLine($"{userInput} is too low");
-
-            }
-            //else if (userInput > favNumber)
-            {
-                //Console.WriteLine($"{userInput} is too high");
-            }
-            //else
-            {
-                //Console.WriteLine("This is my favorite number!"); 
-            
-            } //while (userInput !=favNumber);
-
-            //Switch-Case
-            Console.WriteLine("What is your favorite subject?");
-
-            string subject = Console.ReadLine();
-
-            switch (subject.ToLower()) 
-            {
-                case "math":
-                case"calculus":
-                    Console.WriteLine("I hate math!");
-                    break;
-                case "science":
-                    Console.WriteLine("I love science!");
-                    break;
-                case "PE":
-                    Console.WriteLine("PE is the best!!");
-                    break;
-                case "history":
-                    Console.WriteLine("History is fun!");
-                    break;
-                case "English":
-                    Console.WriteLine("English is boring!");
-                    break;
-                    default:
-                    Console.WriteLine("I don't know that subject.");
-                    break;
-           
-            }
-        }   
-	
-
-	}
     }
-
+}
